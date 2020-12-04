7768be8a596f956c41f4ae297fef57860354d9bf
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-12-04 10:27:00 -0800

    merge in changes from Na

--- generate.py
+++ generate.py
@@ -16,24 +16,58 @@ logging.info("Beginning extension compilation")
 
 extension_classes = {
     "Occupant": {
-        SKOS.definition: Literal("A building occupant"),
+        SKOS.definition: Literal("Building occupant, which can be an indiviudal occupant or a group of occupants sharing some common attributes"),
         "properties": {
             "Permenance": {
                 "exclusive": True,
                 "values": {
-                    "Temporary": {
-                        SKOS.definition: Literal("A temporary building occupant; e.g. a visitor"),
+                    "Individual": {
+                        SKOS.definition: Literal("A description of one individual occupant"),
                     },
-                    "Regular": {
-                        SKOS.definition: Literal("A regular building occupant; e.g. an employee"),
+                    "Group": {
+                        SKOS.definition: Literal("A description of a certain group of occupant"),
                     }
                 }
             },
             "Gender": {
                 "exclusive": False,
-                "values": [
-                    "Male", "Female", "Non-binary", "Decline to state"
-                ],
+                "values": {
+                    "Male": {
+                        SKOS.definition: Literal("Occupants are male"),
+                    },
+                    "Female": {
+                        SKOS.definition: Literal("Occupants are female"),
+                    },
+                    "Nonbinary": {
+                        SKOS.definition: Literal("Occupants are non-binary"),
+                    },
+                    "Decline_to_state": {
+                        SKOS.definition: Literal("Occupants declined to state"),
+                    },
+                }
+            },
+            "Race": {
+                "exclusive": False,
+                "values": {
+                    "African_American": {
+                        SKOS.definition: Literal("Occupants are African American"),
+                    },
+                    "Asian": {
+                        SKOS.definition: Literal("Occupants are Asian"),
+                    },
+                    "American_Indian": {
+                        SKOS.definition: Literal("Occupants are American Indian"),
+                    },
+                    "Native_Hawaiian": {
+                        SKOS.definition: Literal("Occupants are Native Hawaiian"),
+                    },
+                    "White": {
+                        SKOS.definition: Literal("Occupants are white people"),
+                    },
+                    "Mix": {
+                        SKOS.definition: Literal("The group of occupants has a mix of races"),
+                    }
+                }
             },
             # TODO: make AgeRange more general (attach to a group of occupants too), and
             # be able to refer to it so we don't repeat the definition
@@ -60,8 +94,38 @@ extension_classes = {
                 "datatype": XSD.Integer,
             },
             # TODO: Race could be better modeled as Ancestry? Still need an enumeration definition (http://sitn.hms.harvard.edu/flash/2017/science-genetics-reshaping-race-debate-21st-century/)
-            # TODO: thermal preference
             # TODO: energy use style
+            "Thermal_Preference": {
+                "exclusive": False,
+                "values": {
+                    "Warmer": {
+                        SKOS.definition: Literal("Occupants prefer warmer indoor environment"),
+                    },
+                    "Cooler": {
+                        SKOS.definition: Literal("Occupants prefer cooler indoor environment"),
+                    },
+                    "Neutral": {
+                        SKOS.definition: Literal("Occupants prefer neutral indoor environment"),
+                    },
+                    "No_Preference": {
+                        SKOS.definition: Literal("Occupants do not have special thermal preference"),
+                    }
+                }
+            },
+            "Energy_Use_Style": {
+                "exclusive": False,
+                "values": {
+                    "Austerity": {
+                        SKOS.definition: Literal("Occupants have an austerity energy use behavior"),
+                    },
+                    "Wasteful": {
+                        SKOS.definition: Literal("Occupants have a wasteful energy use behavior"),
+                    },
+                    "Norm": {
+                        SKOS.definition: Literal("Occupants have an average energy use behavior"),
+                    }
+                }
+            },
 
         },
         "subclasses": {
@@ -80,15 +144,66 @@ equipment_extensions = {
         SKOS.definition: Literal("Equipment used in the building envelope"),
         "subclasses": {
             "Window": {
-                SKOS.definition: Literal("An opening in the wall or roof of a building or vehicle that is fitted with glass or other transparent material in a frame to admit light or air and allow people to see out."),
+                SKOS.definition: Literal("An opening in the wall of a building to admit light or air and allows occupants to see out"),
             },
+            "Door": {
+                SKOS.definition: Literal("A hinged, sliding, or revolving barrier at the entrance of a building or a zone that allows occupants to go in and out"),
+            }
+        }
+    },
+    "Personal_Device": {
+        SKOS.definition: Literal("Dedicated devices used to maintain thermal comfort for occupants"),
+        "subclasses": {
+            "Portable_Fan": {
+                SKOS.definition: Literal("A cord-connected appliance that is easily moved by hand from place to place to provide flowing air"),
+            },
+            "Portable_Heater": {
+                SKOS.definition: Literal("A cord-connected appliance that is easily moved by hand from place to place to provide heating"),
+            },
+            "Ceiling_Fan": {
+                SKOS.definition: Literal("A mechanical fan mounted on the ceiling of a room or space, usually electrically powered, that uses hub-mounted rotating blades to circulate air"),
+            },
+            "Desktop_Light": {
+                SKOS.definition: Literal("A lighting equipment on the desktop to provide illuminance"),
+            }
+        }
+    }
+}
+point_extensions = {
+    "Status": {
+        SKOS.definition: Literal("Status of the equipment"),
+        "subclasses": {
+            "Window_Status": {
+                SKOS.definition: Literal("Status of the windows (open, closed)"),
+            },
+            "Door_Status": {
+                SKOS.definition: Literal("Status of the doors (open, closed)"),
+            },
+            "Lighting_Status": {
+                SKOS.definition: Literal("Status of the lightings (on, off)"),
+            }
+        }
+    },
+    "Sensor": {
+        SKOS.definition: Literal("A Sensor is an input point that represents the value of a device or instrument designed to detect and measure a variable (ASHRAE Dictionary)"),
+        "subclasses": {
+            "Window_Sensor": {
+                SKOS.definition: Literal("A sensor used to monitor the status of the window (open, closed, open angle)"),
+            },
+            "Wifi_AP": {
+                # TODO: address?
+                SKOS.definition: Literal("Number of wifi access point"),
+            },
+            "Shading_Sensor": {
+                SKOS.definition: Literal("A sensor used to monitor the status of the shading (on, off)"),
+            }
         }
     }
 }
 
-
 define_classes(extension_classes, BRICK.Class, ns=EXT)
 define_classes(equipment_extensions, BRICK.Equipment, ns=EXT)
+define_classes(point_extensions, BRICK.Status, ns=EXT)
 
 # enumerations
 enums = {
@@ -134,11 +249,11 @@ enum_values = {
     },
     "Adjustable": {
         A: EXT.OccupantAccessibilityLevel,
-        SKOS.definition: Literal("Device is accessible to occupants"),
+        SKOS.definition: Literal("Device is accessible to occupants for adjustment"),
     },
     "Shared": {
         A: EXT.OccupantAccessibilityLevel,
-        SKOS.definition: Literal("Device is accessible by many occupants"),
+        SKOS.definition: Literal("Device is accessible by multiple occupants"),
     },
 
     "ManualMode": {
